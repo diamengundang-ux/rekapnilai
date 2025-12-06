4e6a41583c20450281bd52f589b98e8f96d35f42
diamengundang-ux <diamengundang@gmail.com>
2025-12-06 13:26:00 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,10 @@ import App from './App.jsx'
 
 // Kode CSS Reset sederhana agar font lebih rapi (Inter Font)
 const style = document.createElement('style');
-style.innerHTML = `
+style.innerHTML = 
   @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
   body { font-family: 'Inter', sans-serif; }
-`;
+;
 document.head.appendChild(style);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
