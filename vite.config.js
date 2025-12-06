504febbc7f91ea3ad02196f99fd7d0db38bd81d0
diamengundang-ux <diamengundang@gmail.com>
2025-12-06 07:56:36 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Kode CSS Reset sederhana agar font lebih rapi (Inter Font)
-const style = document.createElement('style');
-style.innerHTML = `
-  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
-  body { font-family: 'Inter', sans-serif; }
-`;
-document.head.appendChild(style);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
